6d8c1d656d0d7cd9ebdb495f47e52701468b61e5
AustinRado <radoaussy@gmail.com>
2023-08-31 11:06:16 +0300

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
     return(
         <>
             <h1 className="text-4xl text-green-800">KAMATA</h1>
+            <p>Kamata is an automated attendance system is to computerize the traditional way of recording attendance and provide an efficient and automated method to track attendance of students, classes, and lecturers in institutions.</p>
              <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                  <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                      KAMATA
@@ -11,11 +12,11 @@ const Home = () => {
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
+                    Sign in
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#">
                     <div>
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email</label>
                         <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""></input>
                     </div>
                     <div>
