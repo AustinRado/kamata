a85ce211fa26f7325193bb3acfd979f80a5a87a1
AustinRado <radoaussy@gmail.com>
2023-09-23 11:40:01 +0300

    setting up server

--- server/server.js
+++ server/server.js
@@ -4,3 +4,30 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//connection with mysql Database
+const connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password:"",
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("Error in connection");
+    } else {
+        console.log("Connected");
+    }
+});
+
+//Port listening
+app.listen(8081, () =>(
+    console.log("Listening at port: 8081")
+));
